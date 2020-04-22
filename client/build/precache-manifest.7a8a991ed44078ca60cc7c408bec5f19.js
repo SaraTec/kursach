96026e67a65720201904692c8faf1dc0f61ca070
SaraTec <cosmo.epik@gmail.com>
2020-04-22 19:52:10 +0300

    build

---
+++ client/build/precache-manifest.7a8a991ed44078ca60cc7c408bec5f19.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27d5afe33ec19e97c51ab4200053b32",
+    "url": "/index.html"
+  },
+  {
+    "revision": "130f54cd71077437f95c",
+    "url": "/static/css/2.7efaf445.chunk.css"
+  },
+  {
+    "revision": "0171b6fa4e2b8f1470ad",
+    "url": "/static/css/main.807bac5c.chunk.css"
+  },
+  {
+    "revision": "130f54cd71077437f95c",
+    "url": "/static/js/2.798983e0.chunk.js"
+  },
+  {
+    "revision": "57df2035aa71769ed811d33768b40c67",
+    "url": "/static/js/2.798983e0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0171b6fa4e2b8f1470ad",
+    "url": "/static/js/main.e07b61a3.chunk.js"
+  },
+  {
+    "revision": "ba5713ab8e9498fbf882",
+    "url": "/static/js/runtime-main.a5f9fc38.js"
+  },
+  {
+    "revision": "2f6792f47d4bd91332837a85bdb37932",
+    "url": "/static/media/map-add-point.2f6792f4.svg"
+  },
+  {
+    "revision": "f03fa90e7337a9bfa91f8be0151d2fe2",
+    "url": "/static/media/map-marker-point.f03fa90e.svg"
+  },
+  {
+    "revision": "8d0d9cf97006fce3eb2a4a1fcd8c946d",
+    "url": "/static/media/user.8d0d9cf9.svg"
+  }
+]);
